4ddeb9bffe505d644da9815194378b9a347ed6ef
abrandel <35696762+abrandel@users.noreply.github.com>
2018-02-17 23:03:04 -0500

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,256 +1,96 @@
-'use strict';
+  'use strict';
 
-var startButton = document.getElementById('startButton');
-var callButton = document.getElementById('callButton');
-var hangupButton = document.getElementById('hangupButton');
-callButton.disabled = true;
-hangupButton.disabled = true;
-startButton.onclick = start;
-callButton.onclick = call;
-hangupButton.onclick = hangup;
 
-var startTime;
-var localVideo = document.getElementById('localVideo');
-var remoteVideo = document.getElementById('remoteVideo');
+window.onload = function() {
+    var video_out = document.getElementById("vid-box");
 
-localVideo.addEventListener('loadedmetadata', function() {
-  trace('Local video videoWidth: ' + this.videoWidth +
-    'px,  videoHeight: ' + this.videoHeight + 'px');
-});
-
-remoteVideo.addEventListener('loadedmetadata', function() {
-  trace('Remote video videoWidth: ' + this.videoWidth +
-    'px,  videoHeight: ' + this.videoHeight + 'px');
-});
-
-remoteVideo.onresize = function() {
-  trace('Remote video size changed to ' +
-    remoteVideo.videoWidth + 'x' + remoteVideo.videoHeight);
-
-  if (startTime) {
-    var elapsedTime = window.performance.now() - startTime;
-    trace('Setup time: ' + elapsedTime.toFixed(3) + 'ms');
-    startTime = null;
-  }
-};
-
-var localStream;
-var pc1;
-var pc2;
-var offerOptions = {
-  offerToReceiveAudio: 1,
-  offerToReceiveVideo: 1
-};
-
-function getName(pc) {
-  return (pc === pc1) ? 'pc1' : 'pc2';
-}
-
-function getOtherPc(pc) {
-  return (pc === pc1) ? pc2 : pc1;
-}
-
-function gotStream(stream) {
-  trace('Received local stream');
-  localVideo.srcObject = stream;
-  // Add localStream to global scope so it's accessible from the browser console
-  window.localStream = localStream = stream;
-  callButton.disabled = false;
-}
-while(true) {
-function PicCycle(){
-setTimeout(takepicture(), 3000);
-} }
-function takepicture() {
-    var context = canvas.getContext('2d');
-    if (width && height) {
-      canvas.width = width;
-      canvas.height = height;
-      context.drawImage(video, 0, 0, width, height);
-    
-      var datanew = canvas.toDataURL('image/png');
-      photo.setAttribute('src', data);
-	  processImage(datanew);
-	  if ( VTHacksnewtestsnew.htm >.9)
-	  document.getElementById("lres").innerHTML = "You laughed You lost";
-	  alert("ERROR!!!");
-	  return hangup();
-    } } 
-  else {
-      clearphoto();
+    function login(form) {
+      var phone = window.phone = PHONE({
+        number        : form.username.value || "Anonymous", // listen on username line else Anonymous
+        publish_key   : 'pub-c-da6a1135-eae5-44bb-86f5-3ebdd87d500b',
+        subscribe_key : 'sub-c-445d9000-142a-11e8-91c1-eac6831c625c',
+      });
+      phone.ready(function(){ form.username.style.background="#55ff5b"; });
+      phone.receive(function(session){
+        session.connected(function(session) { video_out.appendChild(session.video); });
+        session.ended(function(session) { video_out.innerHTML=''; });
+      });
+      return false;   // So the form does not submit.
     }
-                        }
+      
+    function makeCall(form){
+      if (!window.phone) alert("Login First!");
+      else phone.dial(form.number.value);
+      return false;
+    }
 
-        function processImage(datanew) {
-            // **********************************************
-            // *** Update or verify the following values. ***
-            // **********************************************
-
-            // Replace the subscriptionKey string value with your valid subscription key.
-            var subscriptionKey = "8d3c23ec9e66474eb8c482a81d11dae6";
-
-            // Replace or verify the region.
-            //
-            // You must use the same region in your REST API call as you used to obtain your subscription keys.
-            // For example, if you obtained your subscription keys from the westus region, replace
-            // "westcentralus" in the URI below with "westus".
-            //
-            // NOTE: Free trial subscription keys are generated in the westcentralus region, so if you are using
-            // a free trial subscription key, you should not need to change this region.
-            var uriBase = "https://westcentralus.api.cognitive.microsoft.com/face/v1.0/detect";
-
-            // Request parameters.
-            var params = {
-                "returnFaceAttributes": "smile,emotion",
-            };
-
-            // Display the image.
-            var sourceImageUrl = datanew;
-            document.querySelector("#sourceImage").src = sourceImageUrl;
-
-            // Perform the REST API call.
-            $.ajax({
-                url: uriBase + "?" + $.param(params),
-
-                // Request headers.
-                beforeSend: function(xhrObj){
-                    xhrObj.setRequestHeader("Content-Type","application/json");
-                    xhrObj.setRequestHeader("Ocp-Apim-Subscription-Key", subscriptionKey);
-                },
-
-                type: "POST",
-
-                // Request body.
-                data: '{"url": ' + '"' + sourceImageUrl + '"}',
-            })
-
-            .done(function(data) {
-                // Show formatted JSON on webpage.
-    		$("#smileval").text(data[0].faceAttributes.smile);
-    		$("#happyval").text(data[0].faceAttributes.emotion.happiness);
-
-        $("#responseTextArea").val(JSON.stringify(data, null, 1));
-
-            })
-            if ((data[0].faceAttributes.smile && data[0].faceAttributes.emotion.happiness)>=.7)
-           console.log(You Lose!)
-
-            .fail(function(jqXHR, textStatus, errorThrown) {
-                // Display error message.
-                var errorString = (errorThrown === "") ? "Error. " : errorThrown + " (" + jqXHR.status + "): ";
-                errorString += (jqXHR.responseText === "") ? "" : (jQuery.parseJSON(jqXHR.responseText).message) ?
-                    jQuery.parseJSON(jqXHR.responseText).message : jQuery.parseJSON(jqXHR.responseText).error.message;
-                alert(errorString);
-            });
-        };
-        */
-
-function onCreateSessionDescriptionError(error) {
-  trace('Failed to create session description: ' + error.toString());
-}
-
-function onCreateOfferSuccess(desc) {
-  trace('Offer from pc1\n' + desc.sdp);
-  trace('pc1 setLocalDescription start');
-  pc1.setLocalDescription(desc).then(
-    function() {
-      onSetLocalSuccess(pc1);
-    },
-    onSetSessionDescriptionError
-  );
-  trace('pc2 setRemoteDescription start');
-  pc2.setRemoteDescription(desc).then(
-    function() {
-      onSetRemoteSuccess(pc2);
-    },
-    onSetSessionDescriptionError
-  );
-  trace('pc2 createAnswer start');
-  // Since the 'remote' side has no media stream we need
-  // to pass in the right constraints in order for it to
-  // accept the incoming offer of audio and video.
-  pc2.createAnswer().then(
-    onCreateAnswerSuccess,
-    onCreateSessionDescriptionError
-  );
-}
-
-function onSetLocalSuccess(pc) {
-  trace(getName(pc) + ' setLocalDescription complete');
-}
-
-function onSetRemoteSuccess(pc) {
-  trace(getName(pc) + ' setRemoteDescription complete');
-}
-
-function onSetSessionDescriptionError(error) {
-  trace('Failed to set session description: ' + error.toString());
-}
-
-function gotRemoteStream(e) {
-  // Add remoteStream to global scope so it's accessible from the browser console
-  window.remoteStream = remoteVideo.srcObject = e.stream;
-  trace('pc2 received remote stream');
-}
-
-function onCreateAnswerSuccess(desc) {
-  trace('Answer from pc2:\n' + desc.sdp);
-  trace('pc2 setLocalDescription start');
-  pc2.setLocalDescription(desc).then(
-    function() {
-      onSetLocalSuccess(pc2);
-    },
-    onSetSessionDescriptionError
-  );
-  trace('pc1 setRemoteDescription start');
-  pc1.setRemoteDescription(desc).then(
-    function() {
-      onSetRemoteSuccess(pc1);
-    },
-    onSetSessionDescriptionError
-  );
-}
-
-function onIceCandidate(pc, event) {
-  if (event.candidate) {
-    getOtherPc(pc).addIceCandidate(
-      new RTCIceCandidate(event.candidate)
-      ).then(
-        function() {
-          onAddIceCandidateSuccess(pc);
-        },
-      function(err) {
-        onAddIceCandidateError(pc, err);
+    function takepicture() { setInterval(function(){ 
+      var context = canvas.getContext('2d');
+      if (width && height) {
+        canvas.width = width;
+        canvas.height = height;
+        context.drawImage(video, 0, 0, width, height);
+        var datanew = canvas.toDataURL('image/png');
+        photo.setAttribute('src', data);
+          if (  processImage(datanew) >= .9)
+          alert("You Lost);
+     
       }
-    );
-    trace(getName(pc) + ' ICE candidate: \n' + event.candidate.candidate);
-  }
-}
+      else {
+        clearphoto();
+      }
+    }, 3000);
+      }
 
-function onAddIceCandidateSuccess(pc) {
-  trace(getName(pc) + ' addIceCandidate success');
-}
-
-function onAddIceCandidateError(pc, error) {
-  trace(getName(pc) + ' failed to add ICE Candidate: ' + error.toString());
-}
-
-function onIceStateChange(pc, event) {
-  if (pc) {
-    trace(getName(pc) + ' ICE state: ' + pc.iceConnectionState);
-    console.log('ICE state change event: ', event);
-  }
-}
-
-
-function trace(text) {
-  if (text[text.length - 1] === '\n') {
-    text = text.substring(0, text.length - 1);
-  }
-  if (window.performance) {
-    var now = (window.performance.now() / 1000).toFixed(3);
-    console.log(now + ': ' + text);
-  } else {
-    console.log(text);
-  }
+    function processImage(datanew) {
+        // **********************************************
+        // *** Update or verify the following values. ***
+        // **********************************************
+        // Replace the subscriptionKey string value with your valid subscription key.
+        var subscriptionKey = "8d3c23ec9e66474eb8c482a81d11dae6";
+        // Replace or verify the region.
+        //
+        // You must use the same region in your REST API call as you used to obtain your subscription keys.
+        // For example, if you obtained your subscription keys from the westus region, replace
+        // "westcentralus" in the URI below with "westus".
+        //
+        // NOTE: Free trial subscription keys are generated in the westcentralus region, so if you are using
+        // a free trial subscription key, you should not need to change this region.
+        var uriBase = "https://westcentralus.api.cognitive.microsoft.com/face/v1.0/detect";
+        // Request parameters.
+        var params = {
+            "returnFaceAttributes": "smile,emotion",
+        };
+        // Display the image.
+        var sourceImageUrl = datanew;
+        // Perform the REST API call.
+        $.ajax({
+            url: uriBase + "?" + $.param(params),
+            // Request headers.
+            beforeSend: function(xhrObj){
+                xhrObj.setRequestHeader("Content-Type","application/json");
+                xhrObj.setRequestHeader("Ocp-Apim-Subscription-Key", subscriptionKey);
+            },
+            type: "POST",
+            // Request body.
+            data: '{"url": ' + '"' + sourceImageUrl + '"}',
+        })
+        .done(function(data) {
+            // Show formatted JSON on webpage.
+    $("#smileval").text(data[0].faceAttributes.smile);
+    $("#happyval").text(data[0].faceAttributes.emotion.happiness);
+   var smile1 = data[0].faceAttributes.smile;
+   var happy1 = data[0].faceAttributes.emotion.happiness;
+   var result1 = happy1 + smile1;
+		return (result1);
+            $("#responseTextArea").val(JSON.stringify(data, null, 1));
+        })
+        .fail(function(jqXHR, textStatus, errorThrown) {
+            // Display error message.
+            var errorString = (errorThrown === "") ? "Error. " : errorThrown + " (" + jqXHR.status + "): ";
+            errorString += (jqXHR.responseText === "") ? "" : (jQuery.parseJSON(jqXHR.responseText).message) ? 
+                jQuery.parseJSON(jqXHR.responseText).message : jQuery.parseJSON(jqXHR.responseText).error.message;
+            alert(errorString);
+        });
+    };
 }
